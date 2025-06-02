a6aa88c1cca2715437fc1a9120213ffb99624d04
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-02 13:58:52 +0530

    variables

---
+++ 03-run-time-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Script name: $0"
+echo "First arg: $1"
+echo "Second arg: $2"
+echo "Third arg: $3"
+echo "All args: $@"
+echo "Total args: $#"
+echo "present wroking directory: $PWD"

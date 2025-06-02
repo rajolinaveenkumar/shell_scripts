4627f4956e44c028ea99ae1a3bfd8627f728abc2
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-02 19:49:55 +0530

    arrays

--- 08-arraylist.sh
+++ 08-arraylist.sh
@@ -1,6 +1,9 @@
 tools=("git" "jenkins")
 tools+=("grafana")
+tools+=("docker")
 echo "All tools: ${tools[@]}"  # Output: git jenkins grafana
 
+echo "All tools: ${tools[0]}" 
 echo "All tools: ${tools[1]}" 
 echo "All tools: ${tools[2]}" 
+echo "All tools: ${tools[3]}" 

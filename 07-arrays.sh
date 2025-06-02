a62c730f48f9e46bddd05776c712d4278accd1a4
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-02 19:46:00 +0530

    arrays

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+Names=("pushpa" "RRR" "RNK")
+# index starts from 0, size is 3
+
+echo "First name: ${Names[0]}"
+echo "Second name: ${Names[1]}"
+echo "Third name: ${Names[2]}"
+
+echo "All name are: ${Names[@]}"

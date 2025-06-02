21ea7675ea28faa3697afb34b2ed9b092073b89b
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-02 13:27:42 +0530

    variables

---
+++ 01-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Ramesh # no space before and after equal
+PERSON2=Suresh
+person3=mahesh
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you man"
+echo "$PERSON2 : I am good, what about you"
+echo "$PERSON1: Where is $person3"
---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1: Hey hi $PERSON2, How are you"
+echo "$PERSON2: Hello $PERSON1, Thanks for asking I am good. What about you"
+echo "$PERSON1: I am good $PERSON2"
+echo "$PERSON2: where is $PERSON3"

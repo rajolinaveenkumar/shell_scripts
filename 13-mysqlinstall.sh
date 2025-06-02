40d5f0d4f59fc6b0729184500befc38cffff1efc
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-02 21:28:55 +0530

    conditions

--- 13-mysqlinstall.sh
+++ 13-mysqlinstall.sh
@@ -5,7 +5,27 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
-    exit 1
+    exit 1 # other than 0
 else 
-    echo "Script name: $0 is executing"
+    echo "Script name: $0 is executing....."
 fi 
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installing MySQL ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installing git ... FAILURE"
+    exit 1
+else
+    echo "Installing git ... SUCCESS"
+fi

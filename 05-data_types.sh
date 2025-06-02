8608318708d03c7034d08fda4945064f3f20773e
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-02 19:38:53 +0530

    ok

--- 05-data_types.sh
+++ 05-data_types.sh
@@ -8,3 +8,4 @@ echo "Script executed at:$TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM of $NUMBER1 and $NUMBER2 is $SUM"
+
---
+++ 06-bollen.sh
@@ -0,0 +1,8 @@
+name=true
+
+if $name;
+then
+    echo "this is true"
+else
+    echo "this is not true"
+fi

692bd1da91b187e6d138c548010923752cfc7493
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-03 13:32:40 +0530

    loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for i in {0..10} # here two ..(dots) means 0 to 10
+for i in {0..5} # here two ..(dots) means 0 to 10
 do
     echo $i
+done
+
+###########################################3
+echo "next loops is"
+
+for tech in aws devops linux ansible 
+do
+    echo $tech
 done

ab7077efc35567cda30785f21ce5679c58cc5614
rajolinaveenkumar <rajolinaveenkumar6@gmail.com>
2025-06-04 18:29:59 +0530

    source

---
+++ 22-script-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ./23-script-2.sh
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you man"
+echo "$PERSON2 : I am good, what about you"
+echo "$PERSON1: Where is $person3"
+
+CHECK
---
+++ 23-script-2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+person3=mahesh
+
+CHECK(){
+if [ $? -ne 0 ]
+then 
+    echo "calling from script-2"
+fi 
+}
